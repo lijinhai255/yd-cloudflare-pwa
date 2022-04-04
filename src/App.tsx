3bc936ef93dab63d9867f26d79b7f47ed1dd9228
lijinhai <jinhai@carbonstop.net>
2022-04-04 18:09:30 +0800

    cs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [title, getTitle] = useState<"">("");
+  useEffect(() => {
+    fetch("https://yd-cloudflare-server-ts.9760414764447.workers.dev/")
+      .then((response: Response) => response?.json())
+      .then((data) => {
+        // let obj = JSON.parse(data);
+        console.log(data);
+        getTitle(data);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>标题</h2>
+      {title}
     </div>
   );
 }
